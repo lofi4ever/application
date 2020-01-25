a7eea1f77c06ec6a7d0f987502cc3ee9a698d403
lofi4ever <gai@one-touch.ru>
2020-01-25 12:34:22 +0300

    backend started

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const express = require('express');
+
+let handler = express();
+
+handler
+  .use(express.static(`${__dirname}/public`))
+  .get('/', (req, res) => {
+    res.sendFile(`${__dirname}/frontend/index.html`);
+  });
+
+http.createServer(handler)
+  .listen(3000, () => console.log('run'));
