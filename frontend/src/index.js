b70df5aee95a33c480c1d922e2be20d87cf89418
lofi4ever <andrey213av@gmail.com>
2020-01-27 00:25:43 +0300

    frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,9 @@
 /* Scripts */
-import './js/index.js'
+import './js/index.js';
 /* !Scripts */
 
 /* Styles */
-import './scss/main.scss'
+import './scss/main.scss';
 /* !Styles */
 
 console.log('main js file');
